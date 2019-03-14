8d3a9bf75a0bfc19ef8657c39a9eef6dd17bf971
Serena Berry <siberry226@gmail.com>
2019-03-14 13:34:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     binding.pry
-    self.split("." || "?" || "!").delete_if {|w| w.size < 2}.size
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
     
   end
 end
